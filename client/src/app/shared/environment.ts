cb3d2506add2c858661e8c4574ce21429a07cf6a
stammath3 <stam.math.12345@gmail.com>
2025-01-05 17:12:39 +0200

    Azure changes.

--- client/src/app/shared/environment.ts
+++ client/src/app/shared/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://your-backend-app.azurewebsites.net/api',
+    apiUrl: 'https://tasks-fullstack-app-a3fnb9f6cyf2cuca.northeurope-01.azurewebsites.net/',
   };
